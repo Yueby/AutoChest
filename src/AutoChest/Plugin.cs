baa0a0814e38029de4bea710745d3446af7c4202
月白 <yueby_sp@qq.com>
2025-09-15 15:23:33 +0800

    Add auto-buy feature for chests in AutoChest plugin
    Introduces automatic chest purchasing by patching ShopItem.CanBuy and Shop.ItemGotBought using Harmony. Adds configuration options for enabling/disabling auto-buy and setting a buy delay. Also removes the unused Config.Build.user.props.template file and adds a reference to Assembly-CSharp.dll in the project file.

--- src/AutoChest/Plugin.cs
+++ src/AutoChest/Plugin.cs
@@ -1,35 +1,78 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using BepInEx.Configuration;
+using BongoCat;
+using HarmonyLib;
+using UnityEngine;
+using System.Collections;
 
 namespace AutoChest;
 
-// Here are some basic resources on code style and naming conventions to help
-// you in your first CSharp plugin!
-// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
-// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
-// https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
-
-// This BepInAutoPlugin attribute comes from the Hamunii.BepInEx.AutoPlugin
-// NuGet package, and it will generate the BepInPlugin attribute for you!
-// For more info, see https://github.com/Hamunii/BepInEx.AutoPlugin
 [BepInAutoPlugin]
 public partial class Plugin : BaseUnityPlugin
 {
     internal static ManualLogSource Log { get; private set; } = null!;
+    
+    // Configuration options
+    internal static ConfigEntry<bool> autoBuyEnabled = null!;
+    internal static ConfigEntry<float> buyDelay = null!;
+    
+    // Singleton instance
+    private static Plugin _instance = null!;
+    private static bool _hasTriedBuy = false;
 
     private void Awake()
     {
-        // BepInEx gives us a logger which we can use to log information.
-        // See https://lethal.wiki/dev/fundamentals/logging
         Log = Logger;
+        _instance = this;
 
-        // BepInEx also gives us a config file for easy configuration.
-        // See https://lethal.wiki/dev/intermediate/custom-configs
+        // Create configuration options
+        autoBuyEnabled = Config.Bind("AutoChest", "Enabled", true, "Enable automatic chest buying");
+        buyDelay = Config.Bind("AutoChest", "BuyDelay", 0.5f, "Buy delay in seconds");
 
-        // We can apply our hooks here.
-        // See https://lethal.wiki/dev/fundamentals/patching-code
-
-        // Log our awake here so we can see it in LogOutput.log file
+        Harmony.CreateAndPatchAll(typeof(Plugin));
         Log.LogInfo($"Plugin {Name} is loaded!");
+    }
+    
+    // Listen to ShopItem's CanBuy method - auto buy if returns true
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ShopItem), nameof(ShopItem.CanBuy))]
+    public static void OnShopItemCanBuy(ShopItem __instance, bool __result)
+    {
+        if (!autoBuyEnabled.Value || _hasTriedBuy || !__result)
+            return;
+            
+        Log.LogInfo("Detected purchasable chest, starting auto buy...");
+        _hasTriedBuy = true;
+        
+        if (buyDelay.Value > 0)
+        {
+            _instance.StartCoroutine(DelayedBuy(__instance));
+        }
+        else
+        {
+            __instance.Buy();
+        }
+    }
+    
+    // Delayed buy coroutine
+    private static IEnumerator DelayedBuy(ShopItem shopItem)
+    {
+        yield return new WaitForSeconds(buyDelay.Value);
+        
+        if (shopItem != null && shopItem.CanBuy())
+        {
+            Log.LogInfo("Executing automatic chest purchase");
+            shopItem.Buy();
+        }
+    }
+    
+    // Reset buy flag
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Shop), nameof(Shop.ItemGotBought))]
+    public static void OnItemBought()
+    {
+        _hasTriedBuy = false;
+        Log.LogInfo("Chest purchase completed, resetting auto buy flag");
     }
 }
